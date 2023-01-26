a1d625b1f59eb02b522028d0947e99637c2475ce
Angular0527 <Angular0527@outlook.com>
2023-01-26 09:47:36 +0200

    add login and register form additional validation

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -17,7 +17,7 @@ export class CreateUserComponent implements OnInit {
   hide = true;
   constructor(private authService: AuthService, private router: Router, private scheduleService: ScheduleService) {
     this.createAccountForm = new FormGroup({
-    'email': new FormControl('', [Validators.required, Validators.email]),
+    'email': new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     'password':new FormControl('', [Validators.required, Validators.minLength(5)]),
   })}
   authFailed = false;
